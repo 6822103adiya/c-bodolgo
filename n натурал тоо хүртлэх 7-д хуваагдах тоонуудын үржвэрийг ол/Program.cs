c49f1c4c62a03ca5978492fad8fb4d06dedaaad5
CodeMulldog <119850314+CodeMulldog@users.noreply.github.com>
2023-03-12 21:04:51 +0800

    Add files via upload
    ur dund aldaa

---
+++ n натурал тоо хүртлэх 7-д хуваагдах тоонуудын үржвэрийг ол/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SevenMultiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("n тоог оруулна уу: ");
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.Write("Буруу утга орууллаа. n тоог дахин оруулна уу: ");
+            }
+            int product = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    product *= i;
+                }
+            }
+            Console.WriteLine("7-д хуваагдах тоонуудын үржвэр:");
+            Console.ReadLine();
+        }
+    }
+}
