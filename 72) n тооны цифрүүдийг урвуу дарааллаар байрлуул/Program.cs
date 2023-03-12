784ba1a568e6be68e15d97edecf49a0813e52ab5
CodeMulldog <119850314+CodeMulldog@users.noreply.github.com>
2023-03-12 20:31:02 +0800

    Add files via upload

---
+++ 72) n тооны цифрүүдийг урвуу дарааллаар байрлуул/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+//code by 103_dev
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Тоо оруул:");
+        int number = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = number % 10;
+            number /= 10;
+        }
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i]);
+            if (i != numbers.Length - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
+//Тоо өгөхдөө 123456 гэж өгсөн бол дараагийн тоо болох 7 гэж бичнэ үү 
